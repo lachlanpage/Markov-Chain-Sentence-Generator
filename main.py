aeb2db8080bfdf4274b1ba3cf0162770b1fed3b0
Lachlan Page <lachlanpage97@gmail.com>
2018-01-24 20:24:37 +1030

    Code Reformat

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
 #Markov Sentence Generator 
-import random 
-tester = ""
-with open("book.txt", 'r') as content_file:
-    tester = content_file.read()
-tester = tester.split()
-tester = tester[:10000]
-#tester = "do you like green eggs and ham? I do like green legs and sam"
-#tester = tester.split()
-#two passes one for finding keys, second pass for finding tokens
+#By Lachlan Page
 
+import random 
+
+file_string = ""
+with open("book.txt", 'r') as content_file:
+    file_string = content_file.read()
+file_string = file_string.split()
+file_string = file_string[:1000]
+
+#full stop in dictionary to handle end cases
 chain = {}
 chain['.'] = ' '
-for key in tester: 
+
+#two passes first for finding keys, second pass for finding tokens
+for key in file_string: 
     if key not in chain:
         chain[key] = []
 
-
-#for value in dictionary, iterate through text for occurance of value, get next word and append to list
-i = 0
+#For token in dict, iterate through file_string for occurance of token, get next word and append to list. If not in bounds append '.'
 for token in chain:
     token_list = []
-    for ind in range(0, len(tester)):
-        if(token == tester[ind]):
-            if(ind+1) >= len(tester):
-                #bout to go out of bounds need to include start and end bits
-                #To fix
+    for i in range(0, len(file_string)):
+        if(token == file_string[i]):
+            if(i+1) >= len(file_string):
+                #If out of bounds include full stop. Should fix this to use something else. 
                 token_list.append(".")
             else:
-                token_list.append(tester[ind+1])
+                token_list.append(file_string[i+1])
     chain[token] = token_list
-    i = i + 1
-    #
 
-WORD_LENGTH = 1000
+#Prediction 
+#Can move to dedicated function or something... 
+WORD_LENGTH = 10
 start_word = random.choice(list(chain.keys()))
 final_word_list = []
 final_word_list.append(start_word)
-
 for i in range(WORD_LENGTH):
     current_word = final_word_list[i]
-    next_word = random.choice(chain[final_word_list[i]])
+    next_word = random.choice(chain[current_word])
     final_word_list.append(next_word)
 
-final = ''
+#Formatting python list to a string for output
+result = ''
 for word in final_word_list:
-    final+= str(word) + " "
-print(final)
+    result+= str(word) + " "
+print(result)
