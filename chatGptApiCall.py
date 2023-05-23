85c7506a85b5675015836ac1c38532fc3b3a324c
tcpiplab <lshep.usc@gmail.com>
2023-05-23 08:43:03 -0700

    Add cleaned up heartOfDarkness.txt from project Gutenberg.

--- chatGptApiCall.py
+++ chatGptApiCall.py
@@ -3,7 +3,7 @@ import requests
 from n_order_markov import generate_text
 from n_order_markov import convert_word_list_to_string
 
-final_word_list = generate_text("bleak-house.txt", 4, 5)
+final_word_list = generate_text("heartOfDarkness.txt", 2, 30)
 
 # Convert the word list to a string
 sentence = convert_word_list_to_string(final_word_list)
@@ -17,8 +17,8 @@ headers = {
 data = {
     "model": "text-davinci-003",
     "prompt": "The following sentence may be missing something: \"" + sentence + "\". "
-              "Please make the sentence make more sense and make the language sound even more 18th century in "
-              "style. And don't return anything but a single sentence. I only want to see one version of the sentence.",
+              "Please make the sentence make more sense" # and make the language sound even more 18th century in "
+              "And don't return anything but a single sentence. I only want to see one version of the sentence.",
     "temperature": 0.7,
     "max_tokens": 50,
     "n": 1,
@@ -30,6 +30,7 @@ if response.status_code == 200:
     corrected_sentence = response.json().get("choices", [{}])[0].get("text", "").strip()
     if corrected_sentence:
 
+        print(sentence)
         print(corrected_sentence)
 
     else:
