3fe529e220cd6a2e93276ddd6985804200f864f2
tcpiplab <lshep.usc@gmail.com>
2023-06-02 07:02:08 -0700

    Make arg parser section more readable

--- mimic.py
+++ mimic.py
@@ -13,18 +13,38 @@ def parse_args():
 
     # Add arguments
     # Add the optional input file argument
-    parser.add_argument("-i", "--input-file", help="Path to the input file (optional)", default=Config.TRAINING_CORPUS)
-    parser.add_argument("-r", "--raw-markov", action="store_true", help="Print the raw Markov result (optional)")
-    parser.add_argument("-c", "--similarity-check", action="store_true", help="Quantify how similar the output is to the original text (optional)")
-    parser.add_argument("-s", "--seed-words", help="Word(s) to seed the Markov search. If not found it will be added to the resulting output. (optional)", default=None)
-    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
-    parser.add_argument("-q", "--quiet", action="store_true", help="Disable logging completely")
-    parser.add_argument("-l", "--length", help="Approximate length of the output (optional)", default=Config.RESULT_LENGTH)
-    parser.add_argument("-m", "--max-tokens", help="Maximum number of tokens to generate. If not specified, "
-                                                   "it increases automatically if you specify length. (optional)",
-                                                    default=Config.MAX_TOKENS)
-    parser.add_argument("-st", "--similarity-threshold", help="Similarity threshold for the similarity check (optional)",
-                                                    default=Config.SIMILARITY_THRESHOLD)
+    parser.add_argument("-i", "--input-file",
+                        help="Path to the input file a.k.a the training text (optional)",
+                        default=Config.TRAINING_CORPUS)
+    parser.add_argument("-r", "--raw-markov",
+                        action="store_true",
+                        help="Print the raw Markov result (optional)")
+    parser.add_argument("-sc", "--similarity-check",
+                        action="store_true",
+                        help="Quantify how similar the output is to the original text (optional)")
+    parser.add_argument("-sw", "--seed-words",
+                        help="Word(s) to seed the Markov search. "
+                             "If not found in the original text, it will be prepended to the output. (optional)",
+                        default=None)
+    parser.add_argument("-v", "--verbose",
+                        action="store_true",
+                        help="Enable verbose mode")
+    parser.add_argument("-q", "--quiet",
+                        action="store_true",
+                        help="Disable logging completely")
+    parser.add_argument("-l", "--length",
+                        help="Approximate length of the output (optional)",
+                        default=Config.RESULT_LENGTH)
+    parser.add_argument("-m", "--max-tokens",
+                        help="Maximum number of tokens to generate. If not specified, "
+                             "it increases automatically if you specify length. (optional)",
+                        default=Config.MAX_TOKENS)
+    parser.add_argument("-st", "--similarity-threshold",
+                        help="Similarity threshold for the similarity check (optional)",
+                        default=Config.SIMILARITY_THRESHOLD)
+    parser.add_argument("-sw", "--similarity-window",
+                        help="Similarity window for the similarity check (optional)",
+                        default=Config.SIMILARITY_WINDOW)
     # TODO:  Add the optional test argument
     # parser.add_argument("-t", "--test", action="store_true", help="Test the API call")
 
