bfeb9960b231d3f589f9c71c5060ec82ac550309
tcpiplab <lshep.usc@gmail.com>
2023-06-17 08:51:41 -0700

    Add comments and clean up

--- text_utilities.py
+++ text_utilities.py
@@ -20,7 +20,8 @@ class TextGenerator:
     # This method is used to return the text from a given corpus file. The corpus file should be in UTF-8 format. But
     # Combining both the errors='replace' parameter and the try/except block provides more robust error handling for
     # various issues that may arise when working with input files.
-    def return_corpus_text(self, corpus_file_name):
+    @staticmethod
+    def return_corpus_text(corpus_file_name):
         """
                Reads the content of a corpus file and returns it as a string.
 
@@ -46,7 +47,8 @@ class TextGenerator:
             return ""
 
     #  This method is used to convert the list of words into a string from a given corpus file.
-    def convert_word_list_to_string(self, final_output_word_list):
+    @staticmethod
+    def convert_word_list_to_string(final_output_word_list):
         return ' '.join(final_output_word_list)
 
     def generate_text(self, corpus_file_name, prefix_length, output_words_length, seed_words=None):
@@ -62,7 +64,8 @@ class TextGenerator:
 
         return output_word_list
 
-    def clean_up_markov_output(self, output_word_list):
+    @staticmethod
+    def clean_up_markov_output(output_word_list):
         # This method is used to clean up the output of the Markov algorithm.
 
         # Remove words that shouldn't be at the end of a sentence.
@@ -77,12 +80,22 @@ class TextGenerator:
         # prefix_length determines which order of Markov
         # meaning how many words to look backwards to predict the next word.
         chain = {tuple(['.'] * prefix_length): [' ']}
+
+        # Iterate through the corpus and create a dictionary of sequences and their next words.
         for i in range(len(corpus_as_string) - prefix_length):
+
+            # the variable seq holds a tuple of characters, representing the sequence of length prefix_length
+            # starting at index i in the input string corpus_as_string.
             seq = tuple(corpus_as_string[i: i + prefix_length])
+
+            # Initialize an empty list as the value for a new seq key in the chain dictionary
+            # if it doesn't already exist.
             if seq not in chain:
                 chain[seq] = []
+
+            # If the next word is not a period, add it to the chain.
             chain[seq].append(corpus_as_string[i + prefix_length] if i + prefix_length < len(corpus_as_string) else '.')
-        # start_seq = None
+
         if seed_words is not None:
             start_seq = tuple(seed_words.split())
         else:
@@ -107,7 +120,8 @@ class TextGenerator:
             output_word_list.append(next_word)
         return output_word_list
 
-    def clean_up_corpus_string(self, corpus_as_string):
+    @staticmethod
+    def clean_up_corpus_string(corpus_as_string):
         corpus_as_string = corpus_as_string.lower()
         for char in ["!", ".", ",", "@", "&", "?", "-"]:
             corpus_as_string = corpus_as_string.replace(char, " ")
